fce559ff145642ab461c348f85e0778a90c70687
Edvard Thörnros <edvard.thornros@gmail.com>
2019-09-09 20:19:59 +0200

    Clang Format

--- src/util/memory.h
+++ src/util/memory.h
@@ -1,22 +1,24 @@
 
 namespace Util {
-    struct MemoryArena {
-        u64 watermark;
-        MemoryArena *next;
-        void *memory;
 
-        template<typename T> T*
-            push(u64 count=1);
+struct MemoryArena {
+    u64 watermark;
+    MemoryArena *next;
+    void *memory;
 
-        void pop();
-    };
+    template <typename T>
+    T *push(u64 count = 1);
 
-    constexpr u64 TOTAL_MEMORY_BUDGET = 1 << 29; // ~0.5GB
-    constexpr u64 ARENA_SIZE_IN_BYTES = 1 << 19; // ~0.5MB
-    constexpr u64 NUM_ARENAS = TOTAL_MEMORY_BUDGET / ARENA_SIZE_IN_BYTES; // ~0.5MB
-    struct GlobalMemoryBank {
-        u64 num_free_regions;
-        MemoryArena *free_regions;
-        MemoryArena all_regions[NUM_ARENAS];
-    } global_memory;
-}
+    void pop();
+};
+
+constexpr u64 TOTAL_MEMORY_BUDGET = 1 << 29;                           // ~0.5GB
+constexpr u64 ARENA_SIZE_IN_BYTES = 1 << 19;                           // ~0.5MB
+constexpr u64 NUM_ARENAS = TOTAL_MEMORY_BUDGET / ARENA_SIZE_IN_BYTES;  // ~0.5MB
+struct GlobalMemoryBank {
+    u64 num_free_regions;
+    MemoryArena *free_regions;
+    MemoryArena all_regions[NUM_ARENAS];
+} global_memory;
+
+}  // namespace Util
