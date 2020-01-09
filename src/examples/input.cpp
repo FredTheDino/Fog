1b63e9d553d1c9b012efec1c315c220c770e1726
Gustav Sörnäs <gusso230@student.liu.se>
2020-01-09 23:28:39 +0100

    Revert "Fix input inconsistency"
    This reverts commit 91690c2e08939ae8a9ed51e6d3ae8bb6f025f166.

--- src/examples/input.cpp
+++ src/examples/input.cpp
@@ -4,30 +4,24 @@
 // To add an input mapping, first add the name for the input in the struct
 // "Input::Name" in "/src/engine/platform/input.h". Then the rest is simple,
 // just remember to call it in the initialization function for the game.
-Input::add(K(a), Input::Name::MY_INPUT);
-Input::add(K(LEFT), Input::Name::MY_INPUT, Input::Player::P2);
+Input::add(K(a), Input::Player::P1, Input::Name::MY_INPUT);
+Input::add(K(LEFT), Input::Player::P2, Input::Name::MY_INPUT);
 // This call adds a new mapping, mapping the key "a" on the keyboard
 // to player 1's "MY_INPUT" and mapping left arrow on the keyboard to
 // player 2's "MY_INPUT". To see if the input was activated this
 // frame, simply call:
-if (Input::pressed(Input::Name::MY_INPUT)) {
+if (Input::pressed(Input::Player::P1, Input::Name::MY_INPUT)) {
     do_stuff();
 }
-if (Input::down(Input::Name::MY_INPUT, Input::Player::P2)) {
+if (Input::pressed(Input::Player::ANY, Input::Name::MY_INPUT)) {
     do_more_stuff();
 }
-// <p>
-// "do_stuff" will be called if the "a" or "left arrow" key was pressed
-// THIS FRAME. Since we don't specify a player, it checks for any player.
-// </p>
-// <p>
-// "do_more_stuff" will be called while the "left arrow" key is held down, since we
-// add the requirement of it being player 2.
-// </p>
-// Assuming the previous section where
+// "do_stuff" will be called if the "a" key was pressed THIS FRAME, we only
+// check for the input for player 1. And "do_more_stuff" will be called if
+// either "a" or the left arrow key were pressed, since we removed the
+// requirement of what player pressed it.  Assuming the previous section where
 // the mapping was added was called off course.
-// <br>
-// <span class="note"></span>
+//
 // It might be a good idea to "using namespace Input;" if you are going
 // to do a lot of input calls, since it quite quickly becomes very noise.
 
