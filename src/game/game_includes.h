613d278be4c0634016a010c6deee9739f500ddf2
Edvard Thörnros <edvard.thornros@gmail.com>
2020-02-23 11:16:42 +0100

    Comment options in game_includes.h

--- src/game/game_includes.h
+++ src/game/game_includes.h
@@ -2,14 +2,24 @@
 
 // NOTE(ed): There are currently no alternatives here...
 #define OPENGL_RENDERER
+#define SDL
+
+// Changes how each slice of the texture atlas is created.
 #define OPENGL_TEXTURE_WIDTH 512
 #define OPENGL_TEXTURE_HEIGHT 512
 #define OPENGL_TEXTURE_DEPTH 256
+
+// The number of drawable sprite layers.
 #define OPENGL_NUM_LAYERS 16
+
+// The number of cameras that can be rendered from.
 #define OPENGL_NUM_CAMERAS 2
+// Automatically updates cameras to match the windows
+// aspect ratio. Might not be desired if using multiple
+// cameras.
 #define OPENGL_AUTO_APPLY_ASPECTRATIO_CHANGE true
+// The furthest layer to draw on.
 #define MAX_LAYER (OPENGL_NUM_LAYERS - 2)
-#define SDL
 
 // If the mouse should warp around the screen allowing you to continue scrolling
 // values when tweaking.
