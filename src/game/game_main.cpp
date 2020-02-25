c6ff3cf3d898aedb4b7e3a6b09d364a56f1e8217
Gustav Sörnäs <gusso230@student.liu.se>
2020-02-25 23:10:59 +0100

    Add particle-showcase to game_main

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -3,28 +3,48 @@
 
 namespace Game {
 
+Renderer::ParticleSystem system;
+
 void entity_registration() {}
 
-Physics::ShapeID shape;
-Physics::Body body;
+void setup(int argc, char **argv) {
+    using namespace Input;
+    add(K(a), Name::LEFT);
+    add(K(d), Name::RIGHT);
+    add(K(w), Name::UP);
+    add(K(s), Name::DOWN);
 
-void setup(int argc, char ** argv) {
-    shape = Physics::add_shape_from_sprite(Res::SOME_SPRITE_NAME);
-    body = Physics::create_body(shape, 0.0);
-    body.offset = V2(-0.5, 0.0);
+    Renderer::fetch_camera(0)->zoom = 2;
+
+    system = Renderer::create_particle_system(5, 20, V2(0, 0));
+    system.keep_alive = true;
+    system.one_color = false;
+    system.one_size = false;
+    system.drop_oldest = true;
+    system.alive_time = {0.5, 0.5};
+    system.velocity_dir = {0, 2*PI};
+    system.spawn_size = {0, 0};
+    system.die_size = {0, 0};
+    system.spawn_size_deriv = {0.2, 0.2};
+    system.die_size_deriv = {0, 0};
+    system.die_red = {0.96, 0.96};
+    system.die_green = {0.894, 0.894};
+    system.die_blue = {0.529, 0.529};
+    system.velocity = {0.2, 0.4};
 }
 
 // Main logic
-void update(f32 delta) {}
+void update(f32 delta) {
+    system.update(delta);
+
+    using namespace Input;
+    if (down(Name::LEFT))
+        system.spawn();
+}
 
 // Main draw
 void draw() {
-    static f32 rotation = 0.0;
-    Util::tweak("rotation", &rotation);
-    body.rotation = rotation;
-    Physics::debug_draw_body(&body);
-    Renderer::push_sprite(0, Res::SOME_SPRITE_NAME, V2(0, 0),
-                   V2(1, 1), rotation);
+    system.draw();
 }
 
 }  // namespace Game
