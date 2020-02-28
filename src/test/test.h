189977ce833635e3840272c0e25f17ad70381d35
Gustav Sörnäs <gusso230@student.liu.se>
2020-02-28 11:31:03 +0100

    Add documentation for creating and adding tests

--- src/test/test.h
+++ src/test/test.h
@@ -1,3 +1,41 @@
+#ifdef _COMMENTS_
+///# Tests
+//
+// The engine comes with a built-in test suite. The tests can be run with
+// <code>make test</code> to verify that everything works the way it should.
+// You can add your own tests by first creating a function which returns a
+// <code>Result</code> according to the table below, and then adding a call to
+// it in the <code>tests</code>-array in <code>test_main.cpp</code>.</br>
+//
+// <table class="definition-table">
+// <tr><th>Result</th><th>Meaning</th></tr>
+// <tr><td>PASS</td><td>The test passed and worked as expected.</td></tr>
+// <tr><td>FAIL</td><td>The test failed, something didn't work as expected.</td></tr>
+// <tr><td>FAIL_EXT</td><td>Something in the test's setup failed, so the test
+// wasn't run.</td></tr>
+// <tr><td>SKIP</td><td>The test is not yet implemented, or skipped for some
+// other reason.</td></tr>
+// </table></br>
+
+//// Creating tests
+//
+Result math_addition() {
+    return 1 + 1 == 2
+        && 1 + 0 == 1
+        ? PASS : FAIL;
+}
+
+//// Registering tests
+//
+/* test_main.cpp */
+UnitTest tests[] = {
+    // ...
+    PASS(math_addition),
+    // ...
+};
+
+#endif
+
 namespace Test {
 
 enum Result {
@@ -18,4 +56,5 @@ struct UnitTest {
 #define PASS(func) { STR(func), false, false, func }
 #define FAIL(func) { STR(func), true, false, func }
 #define SKIP(func) { STR(func), true, true, func }
-}
+
+}  // namespace Test
