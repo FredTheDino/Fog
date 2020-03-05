c351d92e66b6a9c5560be1f00ccd8eea0178d5c3
Edvard Thörnros <edvard.thornros@gmail.com>
2020-03-05 19:03:57 +0100

    Refactor and #doStuff

--- src/engine/asset/asset.h
+++ src/engine/asset/asset.h
@@ -19,16 +19,10 @@ namespace Asset {
 // actual number might change randomly
 // </p>
 
-FOG_HIDE
 ///* AssetID
 // An AssetID is a simple and easy way to identify an asset, they are unique
 // and created by "src/fog_assets.cpp"
-using AssetID = u64;
-
-#if 0
-FOG_EXPORT
 typedef u64 AssetID;
-#endif
 
 FOG_EXPORT
 const AssetID ASSET_ID_NO_ASSET = 0xFFFFFFFF;
