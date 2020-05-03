dc474923c1a54bb256738225fcccc0316699508a
Gustav Sörnäs <gustav@sornas.net>
2020-05-03 20:35:03 +0200

    Update logic example

--- src/examples/logic.cpp
+++ src/examples/logic.cpp
@@ -1,33 +1,26 @@
 ///# Logic examples
-// <span class="note"></span>
-// This hasn't yet been updated with C-linkage in mind.
-// <br>
 // Some examples showing how to interface with the
-// logic system, adding and removing callbacks and
-// making the use of them practical and efficent.
+// logic system and adding and removing callbacks.
 
 //// Modifying every 3rd second
 // <p>
-// Lambda functions can be added to the update loop,
-// they can thus be used to time, delay or just simply
-// deligate update calls.
-// </p>
-// <p>
-// This example modifies the variable <i>score</i> every
+// This example modifies the variable <code>score</code> every
 // third second and increases it by 1. Thus you only
 // have to call add_callback once to call the callback
 // until the end of time.
 // </p>
 int score = 0;
-Function callback = [&score](){
+void increase_score(f32 timestep, f32 delta, f32 progress, void *aux) {
     score++;
-    LOG("I do stuff every third second!");
-};
-LogicID id = Logic::add_callback(Logic::At::PRE_DRAW, callback, 1.0,
-                                 Logic::FOREVER, 3.0);
+    printf("I do stuff every third second!\n");
+}
+/*
+ * somewhere else
+ */
+LogicID id = fog_logic_add_callback(PRE_UPDATE, increase_score, 1.0, -1, 3.0, NULL);
 // <p>
-// Here the score is increased every third second forever. This is usefull if you
-// want something to happen at some intervals. If you want to remove the id, simply
-// call the remove function.
+// Here the score is increased every third second forever. This is useful if you
+// want something to happen at some regular interval. If you want to remove the
+// callback, simply call the remove function.
 // </p>
-Logic::remove_callback(id);
+fog_logic_remove_callback(id);
